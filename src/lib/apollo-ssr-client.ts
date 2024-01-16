8cf7ba522e5eb8ab8148ce21a97a5cf4e9d2bb18
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-01-16 00:53:40 -0300

    🔧 add cookies to request

--- src/lib/apollo-ssr-client.ts
+++ src/lib/apollo-ssr-client.ts
@@ -1,15 +1,13 @@
-import { HttpLink } from "@apollo/client";
-import {
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-} from "@apollo/experimental-nextjs-app-support/ssr";
-import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { HttpLink } from '@apollo/client';
+import { NextSSRInMemoryCache, NextSSRApolloClient } from '@apollo/experimental-nextjs-app-support/ssr';
+import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
 
 export const { getClient } = registerApolloClient(() => {
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+      credentials: 'include',
     }),
   });
 });
