2ea28e9b1cf8fcfc09cc0e6de160c17635597582
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-01-18 23:05:28 -0300

    💄 update default styles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -17,17 +17,17 @@ function Button({ children, className, variant, variantSize, ...props }: ButtonP
 }
 export default Button;
 
-const buttonVariants = cva('rounded-md', {
+const buttonVariants = cva('rounded-md w-full', {
   variants: {
     variant: {
-      primary: 'border-2 border-black text-black bg-white hover:bg-neutral-200',
-      secondary: 'border-2 border-white text-white bg-black hover:bg-neutral-800',
+      primary: 'border-2 border-solid border-[color:(#475569)] text-white bg-[#070B15] hover:bg-neutral-800',
+      secondary: 'border-2 border-black text-black bg-white hover:bg-neutral-200',
       danger: 'border-none text-white bg-red-500 hover:bg-red-600',
     },
     variantSize: {
       sm: 'text-sm px-1 py-0',
       md: 'text-base px-2 py-0',
-      lg: 'text-xl px-4 py-2',
+      lg: 'text-xl px-3 py-2',
     },
   },
   defaultVariants: {
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -29,20 +29,23 @@ function Input({ children, className, variant, variantSize, register, errors, na
 }
 export default Input;
 
-const inputVariants = cva('rounded-md', {
-  variants: {
-    variant: {
-      primary: 'text-gray-950 rounded focus-visible:outline focus-visible:outline-teal-500',
-      secondary: 'border-white text-white bg-black hover:bg-neutral-800',
+const inputVariants = cva(
+  'rounded outline outline-1 outline-[color:#475569] bg-[#182138] text-[color:E3E7EE] text-sm font-normal p-3 w-full',
+  {
+    variants: {
+      variant: {
+        primary: 'focus-visible:outline focus-visible:outline-teal-500',
+        secondary: 'border-white text-white bg-black hover:bg-neutral-800',
+      },
+      variantSize: {
+        sm: 'text-sm',
+        md: 'text-base',
+        lg: 'text-xl',
+      },
     },
-    variantSize: {
-      sm: 'text-sm p-1 outline-1',
-      md: 'text-base p-2 outline-2',
-      lg: 'text-xl p-3 outline-4',
+    defaultVariants: {
+      variant: 'primary',
+      variantSize: 'md',
     },
   },
-  defaultVariants: {
-    variant: 'primary',
-    variantSize: 'md',
-  },
-});
+);
