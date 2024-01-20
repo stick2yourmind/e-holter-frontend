ae14532cfceb271efa1ad47475b6fce5d41f859e
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-01-20 03:05:13 -0300

    🤡 mock pressure records

---
+++ src/app/record/page.tsx
@@ -0,0 +1,18 @@
+import RecordCard from '@/components/RecordCard/RecordCard';
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center bg-[#070B15] text-gray-100 h-screen">
+      <section className="flex flex-col gap-8">
+        <h2 className="text-2xl">Blood pressure measurement</h2>
+        <div className="grid grid-cols-4 gap-6">
+          <RecordCard minPressure={85} maxPressure={120} pulse={91} date="2024-01-20T05:32:20.000Z" />
+          <RecordCard minPressure={87} maxPressure={130} pulse={94} date="2024-01-18T07:32:20.000Z" />
+          <RecordCard minPressure={92} maxPressure={135} pulse={99} date="2024-01-14T07:32:20.000Z" />
+          <RecordCard minPressure={88} maxPressure={133} pulse={90} date="2024-01-13T07:32:20.000Z" />
+          <RecordCard minPressure={91} maxPressure={140} pulse={93} date="2024-01-12T07:32:20.000Z" />
+        </div>
+      </section>
+    </main>
+  );
+}
