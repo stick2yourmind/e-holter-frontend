80ae5b83fa11f1fc85b4c51c430f968397862da0
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-01-26 20:41:37 -0300

    🔨 render user records at record page

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -8,6 +8,7 @@ import RecordCard from '@/components/RecordCard/RecordCard';
 import './timepicker.css';
 import RecordForm from '@/components/RecordForm/RecordForm';
 import useCreateRecord from './useCreateRecord';
+import useGetRecords from './useGetRecords';
 
 const defaultValues = {
   date: dayjs().format('YYYY-MM-DD'),
@@ -15,11 +16,13 @@ const defaultValues = {
 };
 
 export default function Records() {
+  const { records, loading } = useGetRecords();
+  console.log('🚀 ~ file: page.tsx:20 ~ Records ~ records:', records);
   const { onCreateRecord } = useCreateRecord();
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-[#070B15] text-gray-100">
-      <Modal>
+      {/* <Modal>
         <div className="absolute flex h-full w-full items-center justify-center bg-[#070000bb]">
           <RecordForm
             initialValues={defaultValues}
@@ -27,7 +30,7 @@ export default function Records() {
             submitHandler={onCreateRecord}
           />
         </div>
-      </Modal>
+      </Modal> */}
       <section className="flex flex-col gap-8">
         <h2 className="text-2xl">Blood pressure measurement</h2>
         <button
@@ -38,11 +41,15 @@ export default function Records() {
           <span>CREATE RECORD</span>
         </button>
         <div className="grid grid-cols-4 gap-6">
-          <RecordCard minPressure={85} maxPressure={120} pulse={91} date="2024-01-20T05:32:20.000Z" />
-          <RecordCard minPressure={87} maxPressure={130} pulse={94} date="2024-01-18T07:32:20.000Z" />
-          <RecordCard minPressure={92} maxPressure={135} pulse={99} date="2024-01-14T07:32:20.000Z" />
-          <RecordCard minPressure={88} maxPressure={133} pulse={90} date="2024-01-13T07:32:20.000Z" />
-          <RecordCard minPressure={91} maxPressure={140} pulse={93} date="2024-01-12T07:32:20.000Z" />
+          {records?.map((record) => (
+            <RecordCard
+              key={record.id}
+              minPressure={record.minPressure}
+              maxPressure={record.maxPressure}
+              pulse={record.heartRate}
+              date={record.date}
+            />
+          ))}
         </div>
       </section>
     </main>
